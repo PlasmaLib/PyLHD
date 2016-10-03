c978c306373887ea69aeb704cafea01d8b1a1c02
keisukefujii <fujiisoup@gmail.com>
2016-10-03 17:44:44 +0900

    Docs improved.

--- PyLHD/eg.py
+++ PyLHD/eg.py
@@ -2,18 +2,16 @@
 A function that reads eg file for the LHD experiment.
 The typical usage is
 
->>> data = eg.loadtxt(filename)
+>>> data = eg.load(filename)
 
-The dimension data is stored in self.dim (ordered dict), while the data are
+The dimension axis is stored in self.dim (ordered dict), while the data are
 stored self.val (ordered dict)
 
 To check the contents,
 
 >>> data.dim.keys()
-(equivalently data.dim_keys())
 
 >>> data.val.keys()
-(equivalently data.val_keys())
 
 To access the data,
 
@@ -24,6 +22,7 @@ To access the data,
 can be used.
 
 Created on Mar 28, 2016
+Updated on Oct 3, 2016
 
 @author: keisukefujii
 '''
